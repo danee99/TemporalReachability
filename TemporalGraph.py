3deff548a0d39e6babc8c0969140cbe2a107b934
danee99 <wrk.daniel.degenstein@gmail.com>
2022-05-02 16:43:54 +0200

    despair

--- TemporalGraph.py
+++ TemporalGraph.py
@@ -6,7 +6,8 @@ import numpy as np
 
 class TemporalGraph:
     def __init__(self, nodelist, edgelist):
-        self.nodelist = set()
+        self.nodelist = {}
+        # self.outdegree = []
         self.edgelist = []
 
     # outputs the set of nodes and every temporal edge
@@ -14,10 +15,12 @@ class TemporalGraph:
         for i in self.edgelist:
             print(*i)
         print(self.nodelist)
+        # print(self.outdegree)
 
     # Assumption: Input graph is in edge stream representation, i.e. edges are sorted by timestamps
     def import_edgelist(self, file_name):
         with open('edge-lists\\' + file_name, "r") as f:
+            # self.outdegree = []
             for line in f:
                 arr = line.split()
                 u = int(arr[0])
@@ -27,16 +30,46 @@ class TemporalGraph:
                     l = int(arr[3])
                 except IndexError:
                     l = 1
-                if u != self.nodelist:
-                    self.nodelist.add(u)
-                if v != self.nodelist:
-                    self.nodelist.add(v)
+                if u not in self.nodelist:
+                    self.nodelist.update({u: 0})
+                    # self.nodelist.append(u)
+                    # self.outdegree.append(0)
+                if v not in self.nodelist:
+                    self.nodelist.update({v: 0})
+                    # self.nodelist.append(v)
+                    # self.outdegree.append(0)
+                # self.outdegree[u] = self.outdegree[u] + 1
+                self.nodelist[u] = self.nodelist[u] + 1
                 self.edgelist.append((u, v, t, l))
+        self.nodelist = {k: v for k, v in sorted(self.nodelist.items(), key=lambda item: item[1], reverse=True)}
+
+    # O(n+m)
+    def calc_reachable_nodes(self, node, a, b):
+        reach_num = 1
+        min_at = [np.inf for i in range(0, len(self.nodelist))]
+        min_at[node] = 0
+        for (u, v, t, l) in self.edgelist:
+            if t < a or t + l > b: continue
+            if min_at[u] <= t:
+                if min_at[v] > t + 1:
+                    min_at[v] = t + 1
+                    reach_num = reach_num + 1
+        return reach_num
+
+    # O(n^2 + nm)
+    def total_reachability(self, a, b):
+        result = []
+        for node in self.nodelist:
+            result.append(self.calc_reachable_nodes(node, a, b))
+        # print(result)
+        return sum(result)
 
     # calculates the total reachability of the entire graph after deleting "node"
-    def calc_total_reachability_after(self, a, b, node, my_heap, k):
-        total = 0
-        for x in self.nodelist:
+    def total_reachability_after(self, a, b, node, my_heap, k):
+        result = 0
+        count = 0
+        for x in self.nodelist.keys():
+            count = count + 1
             reach_num = 1
             if x == node:
                 reach_num = 0
@@ -49,32 +82,27 @@ class TemporalGraph:
                         if min_at[v] > t + 1:
                             min_at[v] = t + 1
                             reach_num = reach_num + 1
-            total = total + reach_num
+            result = result + reach_num
             if my_heap != [] and len(my_heap) >= k:
-                if -total < my_heap[0][0]:
-                    print("INNER LOOP abgebrochen bei " + str(x) + "--> " + str(total))
+                if -result < my_heap[0][0]:
                     return -1
-        print("loop passed nach " + str(x) + "--> " + str(total))
-        return total
+        return result
 
     # outputs the top k nodes
     def top_k_nodes(self, a, b, k):
         start_time = time.time()
         my_heap = []
-        for node in self.nodelist:
-            print("bin bei knoten " + str(node))
-            R = self.calc_total_reachability_after(a, b, node, my_heap, k)
+        for node in self.nodelist.keys():
+            R = self.total_reachability_after(a, b, node, my_heap, k)
             if R == -1:
-                print("OUTER LOOP abgebrochen bei " + str(node) + "--> " + str(R))
                 continue
             if len(my_heap) < k:
                 heapq.heappush(my_heap, (-R, node))
             else:
                 if -R > my_heap[0][0]:
                     heapq.heappushpop(my_heap, (-R, node))
-        print(my_heap, ("--- finished in %s seconds ---" % (time.time() - start_time)))
-
-    #-------------------------------------------------------------------------------------------------------------------
+        return my_heap, ("--- finished in %s seconds ---" % (time.time() - start_time))
+    # -------------------------------------------------------------------------------------------------------------------
     def total_reachability(self, a, b):
         total_reach = 0
         for x in self.nodelist:
@@ -87,7 +115,6 @@ class TemporalGraph:
                     if min_at[v] > t + 1:
                         min_at[v] = t + 1
                         reach_num = reach_num + 1
-            print(x, reach_num)
             total_reach = total_reach + reach_num
         return total_reach
 
@@ -128,12 +155,13 @@ if __name__ == '__main__':
     output = data.split(".")[0] + '-Ranking' + '.txt'
     G = TemporalGraph([], [])
     G.import_edgelist(data)
-    print(G.total_reachability(0, np.inf))
-    # wikipediasg.txt         |  V = 208142 | E = 810702 geschätzt 6 h nur um gesamterreichbarkeit auszurechnen
+    a = 0
+    b = np.inf
+    print(G.top_k_nodes(a, b, 3))
+    # wikipediasg.txt         |  V = 208142 | E = 810702    geschätzt 6 h nur um gesamterreichbarkeit auszurechnen
     # facebook.txt            |  V = 63731  | E = 817036
     # infectious.txt          |  V = 10972  | E = 415912
-    # tij_SFHH.txt            |  V = 3906   | E = 70261 1.3 knoten pro minute --> 50 h
-    # ht09_contact_list.txt   |  V = 5351   | E = 20817 3 knoten pro minute --> 29 h
+    # tij_SFHH.txt            |  V = 3906   | E = 70261     1.3 knoten pro minute --> 50 h
+    # ht09_contact_list.txt   |  V = 5351   | E = 20817     3 knoten pro minute --> 29 h
     # aves-weaver-social.txt  |  V = 445    | E = 1426
     # test.txt                |  V = 7      | E = 18
-    # entweder bessere obere grenze oder knoten nach irgendwas sortieren vorher? knotengrad maybe?
--- Testing.py
+++ Testing.py
@@ -2,7 +2,6 @@ import heapq
 import math
 import time
 from io import StringIO
-
 import numpy as np
 
 
@@ -20,7 +19,7 @@ def show_tree(tree, total_width=60, fill=' '):
             output.write('\n')
         columns = 2 ** row
         col_width = int(math.floor((total_width * 1.0) / columns))
-        output.write(str(n).center(col_width, fill))
+        output.write(str((abs(n[0]), n[1])).center(col_width, fill))
         last_row = row
     print(output.getvalue())
     print('-' * total_width)
@@ -28,25 +27,23 @@ def show_tree(tree, total_width=60, fill=' '):
 
 
 class TemporalGraph:
-    def __init__(self, nodemanager, edgelist):
-        self.nodemanager = {}
-        # keys = node ids --> values = [0 ... n-1]
-        self.nodelist = self.nodemanager.values()
+    def __init__(self, nodelist, edgelist):
+        self.nodelist = {}
+        # self.outdegree = []
         self.edgelist = []
 
-    # outputs the set of nodes and the set of edges
+    # outputs the set of nodes and every temporal edge
     def print_graph(self):
         for i in self.edgelist:
             print(*i)
-        print(self.nodemanager)
         print(self.nodelist)
+        # print(self.outdegree)
 
     # Assumption: Input graph is in edge stream representation, i.e. edges are sorted by timestamps
     def import_edgelist(self, file_name):
-        file = 'edge-lists\\' + file_name
-        with open(file) as fp:
-            val = 0
-            for line in fp:
+        with open('edge-lists\\' + file_name, "r") as f:
+            # self.outdegree = []
+            for line in f:
                 arr = line.split()
                 u = int(arr[0])
                 v = int(arr[1])
@@ -55,18 +52,23 @@ class TemporalGraph:
                     l = int(arr[3])
                 except IndexError:
                     l = 1
-                if u not in self.nodemanager.keys():
-                    self.nodemanager.update({u: val})
-                    val = val + 1
-                if v not in self.nodemanager.keys():
-                    self.nodemanager.update({v: val})
-                    val = val + 1
-                if (self.nodemanager[u], self.nodemanager[v], t, l) not in self.edgelist:
-                    self.edgelist.append((self.nodemanager[u], self.nodemanager[v], t, l))
+                if u not in self.nodelist:
+                    self.nodelist.update({u: 0})
+                    # self.nodelist.append(u)
+                    # self.outdegree.append(0)
+                if v not in self.nodelist:
+                    self.nodelist.update({v: 0})
+                    # self.nodelist.append(v)
+                    # self.outdegree.append(0)
+                # self.outdegree[u] = self.outdegree[u] + 1
+                self.nodelist[u] = self.nodelist[u] + 1
+                self.edgelist.append((u, v, t, l))
+        self.nodelist = {k: v for k, v in sorted(self.nodelist.items(), key=lambda item: item[1], reverse=True)}
 
+    # O(n+m)
     def calc_reachable_nodes(self, node, a, b):
         reach_num = 1
-        min_at = [np.inf for i in range(len(self.nodelist))]
+        min_at = [np.inf for i in range(0, len(self.nodelist))]
         min_at[node] = 0
         for (u, v, t, l) in self.edgelist:
             if t < a or t + l > b: continue
@@ -76,19 +78,26 @@ class TemporalGraph:
                     reach_num = reach_num + 1
         return reach_num
 
-    def calc_total_reachability(self, a, b):
-        total_reach = 0
-        for x in self.nodelist:
-            total_reach = total_reach + self.calc_reachable_nodes(x, a, b)
-        return total_reach
+    # O(n^2 + nm)
+    def total_reachability(self, a, b):
+        # for node in self.nodelist:
+        #     self.nodelist[node] = self.calc_reachable_nodes(node, a, b)
+        # self.nodelist = {k: v for k, v in sorted(self.nodelist.items(), key=lambda item: item[1], reverse=True)}
+        result = []
+        for node in self.nodelist:
+            result.append(self.calc_reachable_nodes(node, a, b))
+        sum(result)
 
-    def total_reachability_after(self, node, a, b):
-        after = 0
-        for x in self.nodelist:
+    # calculates the total reachability of the entire graph after deleting "node"
+    def total_reachability_after(self, a, b, node, my_heap, k):
+        result = 0
+        count = 0
+        for x in self.nodelist.keys():
+            count = count + 1
             reach_num = 1
             if x == node:
                 reach_num = 0
-            min_at = [np.inf for i in range(len(self.nodelist))]
+            min_at = [np.inf for i in range(0, len(self.nodelist))]
             min_at[x] = 0
             for (u, v, t, l) in self.edgelist:
                 if u != node and v != node:
@@ -97,49 +106,106 @@ class TemporalGraph:
                         if min_at[v] > t + 1:
                             min_at[v] = t + 1
                             reach_num = reach_num + 1
-            after = after + reach_num
-        return after
+            result = result + reach_num
+            if my_heap != [] and len(my_heap) >= k:
+                if -result < my_heap[0][0]:
+                    print("Inner LOOP zur Berechnung von R(G-v) abgebrochen nach " + str(
+                        count) + " Knoten weil R(G-v) = " + str(result) + " > " + str(abs(my_heap[0][0])))
+                    return -1
+        print("Inner LOOP abgeschlossen nach " + str(count) + " Knoten --> R(G-v) = " + str(result))
+        return result
 
-    def inner(self, a, b, node, my_heap):
-        R = 0
-        for x in self.nodelist:
-            reach_num = 1
-            if x == node:
-                reach_num = 0
-            min_at = [np.inf for i in range(len(self.nodelist))]
-            min_at[x] = 0
-            for (u, v, t, l) in self.edgelist:
-                if u != node and v != node:
-                    if t < a or t + l > b: continue
-                    if min_at[u] <= t:
-                        if min_at[v] > t + 1:
-                            min_at[v] = t + 1
-                            reach_num = reach_num + 1
-            R = R + reach_num
-            if my_heap != [] and -R < my_heap[0][0]:
-                return -1
-        return R
-
+    # outputs the top k nodes
     def top_k_nodes(self, a, b, k):
         start_time = time.time()
         my_heap = []
-        for node in self.nodelist:
-            R = self.inner(a, b, node, my_heap)
+        for node in self.nodelist.keys():
+            show_tree(my_heap, total_width=60, fill=' ')
+            print("\nbin bei knoten " + str(node))
+            R = self.total_reachability_after(a, b, node, my_heap, k)
             if R == -1:
+                print("--> Der Knoten " + str(node) + " gehört nicht zu top k" + " --> " + str(R))
                 continue
             if len(my_heap) < k:
                 heapq.heappush(my_heap, (-R, node))
             else:
                 if -R > my_heap[0][0]:
                     heapq.heappushpop(my_heap, (-R, node))
-        return my_heap, ("--- finished in %s seconds ---" % (time.time() - start_time))
+        return (my_heap, ("--- finished in %s seconds ---" % (time.time() - start_time)))
+
+    def eins(self, a, b, node, my_heap, k):
+        result = 0
+        count = 0
+        completed = []
+        reach = []
+        for x in self.nodelist.keys():
+            count = count + 1
+            reach_num = 1
+            booo = 0
+            for key in self.nodelist.keys():
+                if key not in completed:
+                    booo = booo + self.nodelist[key]
+            bound = sum(reach) + booo
+            if my_heap != [] and len(my_heap) >= k:
+                if -bound < my_heap[0][0]:
+                    print("HIER abgebrochen nach " + str(
+                        count) + " Knoten weil Bound = " + str(bound) + " > " + str(abs(my_heap[0][0])))
+                    return -1
+            if x == node:
+                reach_num = 0
+            min_at = [np.inf for i in range(0, len(self.nodelist))]
+            min_at[x] = 0
+            for (u, v, t, l) in self.edgelist:
+                if u != node and v != node:
+                    if t < a or t + l > b: continue
+                    if min_at[u] <= t:
+                        if min_at[v] > t + 1:
+                            min_at[v] = t + 1
+                            reach_num = reach_num + 1
+
+            reach.append(reach_num)
+            completed.append(x)
+
+            result = result + reach_num
+            if my_heap != [] and len(my_heap) >= k:
+                if -result < my_heap[0][0]:
+                    print("Inner LOOP zur Berechnung von R(G-v) abgebrochen nach " + str(
+                        count) + " Knoten weil R(G-v) = " + str(result) + " > " + str(abs(my_heap[0][0])))
+                    return -1
+        print("Inner LOOP abgeschlossen nach " + str(count) + " Knoten --> R(G-v) = " + str(result))
+        return result
+
+    def zwei(self, a, b, k):
+        start_time = time.time()
+        my_heap = []
+        for node in self.nodelist.keys():
+            show_tree(my_heap, total_width=60, fill=' ')
+            print("\nbin bei knoten " + str(node))
+            R = self.eins(a, b, node, my_heap, k)
+            if R == -1:
+                print("--> Der Knoten " + str(node) + " gehört nicht zu top k" + " --> " + str(R))
+                continue
+            if len(my_heap) < k:
+                heapq.heappush(my_heap, (-R, node))
+            else:
+                if -R > my_heap[0][0]:
+                    heapq.heappushpop(my_heap, (-R, node))
+        return (my_heap, ("--- finished in %s seconds ---" % (time.time() - start_time)))
 
 
 if __name__ == '__main__':
     # data = input('Edgeliste eingeben: ')
-    data = 'tij_SFHH.txt'
-    output = data.split(".")[0] + '_Ranking' + '.txt'
-    G = TemporalGraph({}, [])
+    data = 'aves-weaver-social.txt'
+    output = data.split(".")[0] + '-Ranking' + '.txt'
+    G = TemporalGraph([], [])
     G.import_edgelist(data)
-    print(G.top_k_nodes(0, np.inf, 4))
-    # aves-weaver-social.edges, test1.txt
+    a = 0
+    b = np.inf
+    print(G.top_k_nodes(a, b, 3))
+    # wikipediasg.txt         |  V = 208142 | E = 810702    geschätzt 6 h nur um gesamterreichbarkeit auszurechnen
+    # facebook.txt            |  V = 63731  | E = 817036
+    # infectious.txt          |  V = 10972  | E = 415912
+    # tij_SFHH.txt            |  V = 3906   | E = 70261     1.3 knoten pro minute --> 50 h
+    # ht09_contact_list.txt   |  V = 5351   | E = 20817     3 knoten pro minute --> 29 h
+    # aves-weaver-social.txt  |  V = 445    | E = 1426
+    # test.txt                |  V = 7      | E = 18
