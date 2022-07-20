8b28291dfc71928f254baaa6c7aabc35780b5516
danee99 <wrk.daniel.degenstein@gmail.com>
2022-07-20 12:46:30 +0200

    push

--- src/Rank-Correlation.py
+++ src/Rank-Correlation.py
@@ -18,7 +18,10 @@ def import_ranking(degree_input, reachability_input, output_name, number_of_node
                     for line in c:
                         arr = line.split(",")
                         betw_rank = float(arr[1])
-                        arr3.append(betw_rank)
+                        if betw_rank < 0:
+                            arr3.append(0)
+                        else:
+                            arr3.append(betw_rank)
                 line1 = a.readlines()
                 line2 = b.readlines()
                 arr1 = np.fromstring(line1[0].strip('[]\n'), dtype=float, sep=',')
@@ -27,17 +30,16 @@ def import_ranking(degree_input, reachability_input, output_name, number_of_node
                     f.write(str(j) + "," + str(arr1[j]) + "," + str(arr2[j]) + "," + str(arr3[j]) + '\n')
 
 
-mydataframe = pd.read_csv(path + "/Dataframes/fb-forum")
-obj = sns.heatmap(data=mydataframe.corr(method='kendall'), annot=True)
-obj.set_xticklabels(obj.get_xticklabels(), rotation=90)
-# plt.show()
-plt.tight_layout()
-plt.savefig(path+'/Plots/fb-forum.svg')
+# mydataframe = pd.read_csv(path + "/Dataframes/fb-forum")
+# obj = sns.heatmap(data=mydataframe.corr(method='kendall'), annot=True)
+# obj.set_xticklabels(obj.get_xticklabels(), rotation=90)
+# plt.tight_layout()
+# plt.savefig(path+'/Plots/fb-forum.svg')
 
-# name = "fb-forum"
-# import_ranking(name + "-Outdegrees.txt",
-#                name + "-Rangliste.txt",
-#                name,
-#                899,
-#                "fb-forum-temporal-betweenness.txt"
-#                )
+name = "fb-messages"
+import_ranking(name + "-Outdegrees.txt",
+               name + "-Rangliste.txt",
+               name,
+               1899,
+               "fb-forum-temporal-betweenness.txt"
+               )
