9759defd2de3c6764c5196643b0669a738fbafc9
danee99 <wrk.daniel.degenstein@gmail.com>
2022-08-18 16:27:41 +0200

    g

--- src/Temporal-Reachability.py
+++ src/Temporal-Reachability.py
@@ -141,15 +141,30 @@ class TemporalGraph:
 
     # ranking all nodes
     def node_ranking(self, a, b, output_name):
+        start_time = time.time()
+        self.calc_total_reachability(a, b)
+        helper = [np.inf for _ in range(self.n)]
+        pool = multiprocessing.Pool(multiprocessing.cpu_count())
+        result_objects = [pool.apply_async(self.rank_node, args=(node, a, b, self.total_reachability, helper))
+                          for node in range(0, self.n)]
+        ranking = [r.get() for r in result_objects]
+        pool.close()
+        pool.join()
+        finish = time.time() - start_time
+        with open(path + output_name, 'w') as f:
+            # ranking.sort(reverse=True)
+            # for i in range(len(ranking)):
+            #     f.write(str(i + 1) + ".Platz: " + str(ranking[i][1]) + "\n")
+            f.write("R(G) = %s" % self.total_reachability + "\n")
+            f.write("abgeschlossen in %s Sekunden" % finish + "\n")
+            f.write("abgeschlossen in %s Minuten" % (finish / 60) + "\n")
+            f.write("abgeschlossen in %s Stunden" % (finish / 3600))
         # start_time = time.time()
         # self.calc_total_reachability(a, b)
+        # ranking = []
         # helper = [np.inf for _ in range(self.n)]
-        # pool = multiprocessing.Pool(multiprocessing.cpu_count())
-        # result_objects = [pool.apply_async(self.rank_node, args=(node, a, b, self.total_reachability, helper))
-        #                   for node in range(0, self.n)]
-        # ranking = [r.get() for r in result_objects]
-        # pool.close()
-        # pool.join()
+        # for node in range(0, self.n):
+        #     ranking.append(self.rank_node(node, a, b, self.total_reachability, helper))
         # finish = time.time() - start_time
         # with open(path + output_name, 'w') as f:
         #     ranking.sort(reverse=True)
@@ -159,21 +174,6 @@ class TemporalGraph:
         #     f.write("abgeschlossen in %s Sekunden" % finish + "\n")
         #     f.write("abgeschlossen in %s Minuten" % (finish / 60) + "\n")
         #     f.write("abgeschlossen in %s Stunden" % (finish / 3600))
-        start_time = time.time()
-        self.calc_total_reachability(a, b)
-        ranking = []
-        helper = [np.inf for _ in range(self.n)]
-        for node in range(0, self.n):
-            ranking.append(self.rank_node(node, a, b, self.total_reachability, helper))
-        finish = time.time() - start_time
-        with open(path + output_name, 'w') as f:
-            ranking.sort(reverse=True)
-            for i in range(len(ranking)):
-                f.write(str(i + 1) + ".Platz: " + str(ranking[i][1]) + "\n")
-            f.write("R(G) = %s" % self.total_reachability + "\n")
-            f.write("abgeschlossen in %s Sekunden" % finish + "\n")
-            f.write("abgeschlossen in %s Minuten" % (finish / 60) + "\n")
-            f.write("abgeschlossen in %s Stunden" % (finish / 3600))
 
 
 if __name__ == '__main__':
