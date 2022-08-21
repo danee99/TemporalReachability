252ad8286a2bd1ff9f94610458064b7d71dce4e0
danee99 <wrk.daniel.degenstein@gmail.com>
2022-08-21 18:18:03 +0200

    g

--- src/Rank-Correlation.py
+++ src/Rank-Correlation.py
@@ -103,7 +103,7 @@ cool = [10, 50, 100, 1000]
 # cool = [10]
 for top_k in cool:
     arr2 = []
-    dataset = "dblp-cite"
+    dataset = "wiki_talk_gl"
     # with open(path + "edge-lists\\" + dataset + "-Heuristik-top-" + str(1000) + ".txt", "r") as h:
     with open(path + "edge-lists\\" + dataset + "-k-Nachbarschaft-Ranking (Digraph)-top-" + str(1000) + ".txt", "r") as h:
         with open(path + "edge-lists\\" + dataset + "-Optimal.txt", "r") as o:
