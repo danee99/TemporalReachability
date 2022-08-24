a53fe43f98dd2f344bc24e60cf04109e02561191
danee99 <wrk.daniel.degenstein@gmail.com>
2022-08-24 15:19:19 +0200

    push

--- src/Test.py
+++ src/Test.py
@@ -1,33 +1,85 @@
 import multiprocessing
 import os
 import time
-import heapq
 import numpy as np
+from heapdict import heapdict
+from fibheap import *
 
 # path = os.path.join(os.getcwd(), os.pardir) + "\\edge-lists\\"
 path = "/home/stud/degenste/BA/TemporalReachability/edge-lists/"
 
 
-# def is_label_dominated(label, Q):
-#     for item in Q:
-#         if (label[1] < item[1] and label[0] >= item[0]) or (item[1] == label[1] and item[0] == label[0]):
-#             return False
-#     return True
+# class MinHeap:
+#     def __init__(self, arr=None):
+#         self.heap = []
+#         self.heap_size = 0
+#         if arr is not None:
+#             self.create_min_heap(arr)
+#             self.heap = arr
+#             self.heap_size = len(arr)
+#
+#     def build_min_heap(self, arr):
+#         n = len(arr)
+#         for i in range(int(n / 2), -1, -1):
+#             self.min_heapify(i, arr, n)
+#
+#     def min_heapify(self, i, arr, size):
+#         left_child = i * 2 + 1
+#         right_child = i * 2 + 2
+#         smallest = i
+#         if left_child < size and arr[left_child][0] < arr[smallest][0]:
+#             smallest = left_child
+#         if right_child < size and arr[right_child][0] < arr[smallest][0]:
+#             smallest = right_child
+#         if smallest != i:
+#             arr[i], arr[smallest] = arr[smallest], arr[i]
+#             self.min_heapify(smallest, arr, size)
+#
+#     def insert(self, item):
+#         self.heap.append(item)
+#         self.heap_size += 1
+#         i = self.heap_size - 1
+#         parent = int(ceil(i / 2 - 1))
+#         while parent >= 0 and self.heap[i][0] < self.heap[parent][0]:
+#             self.heap[i], self.heap[parent] = self.heap[parent], self.heap[i]
+#             i = parent
+#             parent = int(ceil(i / 2 - 1))
+#
+#     def delete(self, i):
+#         if self.heap_size == 0:
+#             print("Heap Underflow!!")
+#             return
+#         self.heap[-1], self.heap[i] = self.heap[i], self.heap[-1]
+#         self.heap_size -= 1
+#         self.min_heapify(i, self.heap, self.heap_size)
+#         return self.heap.pop()
+#
+#     # to do
+#     def decrease_key(self, i):
+#         return self.heap
+#
+#     def extract_min(self):
+#         return self.delete(0)
 
 
 class TemporalGraph:
     def __init__(self):
-        self.nodes = set()
+        self.nodes = []
         self.incidence_list = []
         self.n = 0
         self.m = 0
         self.total_reachability = 0
+
+    def print_graph(self):
+        for node in self.nodes:
+            print(str(node) + ": " + str(self.incidence_list[node]))
 
     def import_edgelist(self, file_name):
         with open(path + file_name, "r") as f:
             n = int(f.readline())
             self.n = n
             self.incidence_list = [[] for _ in range(n)]
+            self.nodes = [i for i in range(self.n)]
             for line in f:
                 arr = line.split()
                 u = int(arr[0])
@@ -37,8 +89,6 @@ class TemporalGraph:
                     l = int(arr[3])
                 except IndexError:
                     l = 1
-                self.nodes.add(u)
-                self.nodes.add(v)
                 self.incidence_list[u].append((u, v, t, l))
                 self.m += 1
 
@@ -46,7 +96,8 @@ class TemporalGraph:
         with open(path + file_name, "r") as f:
             n = int(f.readline())
             self.n = n
-            self.incidence_list = [[] for _ in range(n)]
+            self.incidence_list = [[] for _ in range(self.n)]
+            self.nodes = [i for i in range(self.n)]
             for line in f:
                 arr = line.split()
                 u = int(arr[0])
@@ -56,65 +107,32 @@ class TemporalGraph:
                     l = int(arr[3])
                 except IndexError:
                     l = 1
-                self.nodes.add(u)
-                self.nodes.add(v)
                 self.incidence_list[u].append((u, v, t, l))
                 self.incidence_list[v].append((v, u, t, l))
                 self.m += 2
 
-    # def label_setting_fastest_path_algorithm(self, u):
-    #     Q = []
-    #     heapq.heappush(Q, (0, 0, u))
-    #     d = [np.inf for _ in range(self.n)]
-    #     d[u] = 0
-    #     F = set()
-    #     Pi = [np.inf for _ in range(self.n)]
-    #     while Q:
-    #         (a, s, v) = heapq.heappop(Q)
-    #         if v in F:
-    #             d[v] = a - s
-    #             F.add(v)
-    #         for (v, w, t, l) in self.incidence_list[v]:
-    #             if a <= t:
-    #                 if (a, s, v) == (0, 0, u):
-    #                     label = (t + l, t, w)
-    #                 else:
-    #                     label = (t + l, s, w)
-    #                 # Entferne die labels aus Pi[w] und Q, die dominiert werden 2 * O(n) ?
-    #                 # (a0, s0, v) dominiert (a, s, v) wenn s < s0 und a >= a0, oder s = s0 und a > a0
-    #                 # if label is not dominated: O(n) ?
-    #                 if is_label_dominated(label, Q) == False:
-    #                     heapq.heappush(Q, label)
-    #                     Pi[w].add(label)
-    #     return d
-
-    # calculates the total reachability of the given temporal graph in a time interval [a,b]
     def calc_total_reachability(self, a, b):
         for node in self.nodes:
             visited = set()
             earliest_arrival_time = [np.inf for _ in range(self.n)]
             earliest_arrival_time[node] = a
-            PQ = []
-            heapq.heappush(PQ, (0, node))
-            while PQ:
-                (current_arrival_time, current_node) = heapq.heappop(PQ)
+            PQ = makefheap()
+            fheappush(PQ, (0, node))
+            while PQ.num_nodes:
+                (current_arrival_time, current_node) = fheappop(PQ)
                 visited.add(current_node)
                 for (u, v, t, l) in self.incidence_list[current_node]:
                     if v not in visited:
                         if t < a or t + l > b: continue
                         if t + l < earliest_arrival_time[v] and t >= current_arrival_time:
                             if earliest_arrival_time[v] != np.inf:
-                                try:
-                                    PQ.remove((earliest_arrival_time[v], v))
-                                    heapq.heapify(PQ) # Das ist lästig
-                                except ValueError:
-                                    heapq.heappush(PQ, (earliest_arrival_time[v], v))
+                                earliest_arrival_time[v] = t + l
+                                PQ.decrease_key(v, t + l)
                             else:
-                                heapq.heappush(PQ, (earliest_arrival_time[v], v))
-                            earliest_arrival_time[v] = t + l
+                                earliest_arrival_time[v] = t + l
+                                fheappush(PQ, (t + l, v))
             self.total_reachability += len(visited)
 
-    # ranks the node "x", where the ranking is a floating point number between 0 and 1
     def rank_node(self, x, a, b, before, helper):
         total = 0
         for node in self.nodes:
@@ -123,10 +141,10 @@ class TemporalGraph:
             visited = set()
             earliest_arrival_time = helper.copy()
             earliest_arrival_time[node] = a
-            PQ = []
-            heapq.heappush(PQ, (0, node))
-            while PQ:
-                (current_arrival_time, current_node) = heapq.heappop(PQ)
+            PQ = makefheap()
+            fheappush(PQ, (0, node))
+            while PQ.num_nodes:
+                (current_arrival_time, current_node) = fheappop(PQ)
                 if current_node != x:
                     visited.add(current_node)
                 for (u, v, t, l) in self.incidence_list[current_node]:
@@ -134,35 +152,29 @@ class TemporalGraph:
                         if t < a or t + l > b: continue
                         if t + l < earliest_arrival_time[v] and t >= current_arrival_time:
                             if earliest_arrival_time[v] != np.inf:
-                                try:
-                                    PQ.remove((earliest_arrival_time[v], v))
-                                    heapq.heapify(PQ) # Das ist lästig
-                                except ValueError:
-                                    heapq.heappush(PQ, (earliest_arrival_time[v], v))
+                                earliest_arrival_time[v] = t + l
+                                PQ.decrease_key(v, t + l)
                             else:
-                                heapq.heappush(PQ, (earliest_arrival_time[v], v))
-                            earliest_arrival_time[v] = t + l
+                                earliest_arrival_time[v] = t + l
+                                fheappush(PQ, (t + l, v))
             total += len(visited)
-        return 1 - (total / before)
-        # return total, x
+        return 1 - (total / before), x
 
-    # parallelized node ranking
     def node_ranking(self, a, b, output_name):
         start_time = time.time()
         self.calc_total_reachability(a, b)
         helper = [np.inf for _ in range(self.n)]
         pool = multiprocessing.Pool(multiprocessing.cpu_count())
-        result_objects = [pool.apply_async(self.rank_node, args=(node, a, b, self.total_reachability, helper)) for node
-                          in range(0, self.n)]
+        result_objects = [pool.apply_async(self.rank_node, args=(node, a, b, self.total_reachability, helper))
+                          for node in range(0, self.n)]
         ranking = [r.get() for r in result_objects]
         pool.close()
         pool.join()
         finish = time.time() - start_time
         with open(path + output_name, 'w') as f:
-            # ranking.sort()
-            # for i in range(len(ranking)):
-            #     f.write(str(i+1)+".Platz: "+str(ranking[i][1]) + "\n")
-            f.write(str(ranking) + "\n")
+            ranking.sort(reverse=True)
+            for i in range(len(ranking)):
+                f.write(str(i + 1) + ".Platz: " + str(ranking[i][1]) + "\n")
             f.write("R(G) = %s" % self.total_reachability + "\n")
             f.write("abgeschlossen in %s Sekunden" % finish + "\n")
             f.write("abgeschlossen in %s Minuten" % (finish / 60) + "\n")
@@ -174,7 +186,7 @@ if __name__ == '__main__':
     directed = (input('Soll die Kantenliste als gerichtet betrachtet werden? [y/n]:'))
     a = int(input('Intervall a eingeben: '))
     b = np.inf
-    output_file = input_graph.split(".")[0] + '-PQ-Test' + '.txt'
+    output_file = input_graph.split(".")[0] + '-Optimal-fib-heap.txt'
     G = TemporalGraph()
     if directed == 'y':
         G.import_edgelist(input_graph)
